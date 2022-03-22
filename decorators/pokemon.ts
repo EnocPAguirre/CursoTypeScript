fe2a336892dc62411058cd582abf8e500b3192b9
EnocPAguirre <enocpinedaa@gmail.com>
2022-03-21 23:39:16 -0600

    Factory Decorators

--- decorators/pokemon.ts
+++ decorators/pokemon.ts
@@ -1,3 +1,17 @@
+
+function prinToConsole(constructor: Function){
+    console.log(constructor);
+}
+
+const printToConsoleConditional = (print : boolean = false) : Function =>{
+    if(print){
+        return prinToConsole;
+    }else {
+        return ()=> {}
+    }
+}
+
+@printToConsoleConditional(true)
 export class Pokemon {
      public API: string = 'https://pokeapi.co';
 
